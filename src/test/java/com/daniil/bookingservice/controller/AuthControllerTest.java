2a35e9039430b3b334e58bcef54c2234ff75aaa7
datky <tkachenko.d@cdek.ru>
2025-10-23 20:07:08 +0300

    1

--- src/test/java/com/daniil/bookingservice/controller/AuthControllerTest.java
+++ src/test/java/com/daniil/bookingservice/controller/AuthControllerTest.java
@@ -8,10 +8,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import com.daniil.bookingservice.dto.UserDto;
+import com.daniil.bookingservice.service.AuthService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @WebMvcTest(AuthController.class)
 class AuthControllerTest {
